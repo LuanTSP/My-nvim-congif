134d757b9789f9252aa253be4764398eae8480ae
LuanTSP <luanpacifico@gmail.com>
2023-10-06 21:18:30 -0300

    Added dashboard option

--- lua/LuanTSP/core/keymaps.lua
+++ lua/LuanTSP/core/keymaps.lua
@@ -21,6 +21,7 @@ keymap.set('n', '<leader>fw', ":Telescope live_grep<CR>")
 keymap.set('n', '<leader>fb', ":Telescope buffers<CR>")
 keymap.set('n', '<leader>fh', ":Telescope help_tags<CR>")
 keymap.set('n', '<leader>fg', ":Telescope git_files<CR>")
+keymap.set('n', '<leader>fc', ":Telescope colorscheme<CR>")
 -- barbar
 keymap.set("n", "<leader>x", ":bdelete!<CR>") -- close current buffer tab
 keymap.set("n", "<leader>l", ":BufferLineCycleNext<CR>")
